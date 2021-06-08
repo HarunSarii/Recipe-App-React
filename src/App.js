1eca3c70fbe6471f0299eed380ece4ec88cb70aa
HarunSarii <harunsari14@gmail.com>
2021-06-09 00:51:55 +0300

    initiated recipe-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import { BrowseRouter as Router } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
