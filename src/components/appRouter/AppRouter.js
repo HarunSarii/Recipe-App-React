4ba6828cf9bf7ae49ccae37edca387439959a23f
HarunSarii <harunsari14@gmail.com>
2021-06-24 17:41:32 +0300

    finished recipe app

--- src/components/appRouter/AppRouter.js
+++ src/components/appRouter/AppRouter.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../navbar/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "../home/Home";
 import About from "../about/About";
 import Details from "../details/Details";
+import Login from "../login/Login";
+import PrivateRouter from "./PrivateRouter";
 
 const AppRouter = () => {
+  const [auth, setAuth] = useState(false);
+
+  const AuthContainer = () => (
+    <div>
+      <Navbar />
+      <PrivateRouter auth={auth} exact path="/" component={Home} />
+      <PrivateRouter auth={auth} exact path="/details" component={Details} />
+      <PrivateRouter auth={auth} exact path="/about" component={About} />
+    </div>
+  );
   return (
     <div>
       <Router>
-        <Navbar />
         <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" exact component={About} />
-          {/* <Route path="/details" exact component={Details} /> */}
+          <Route
+            path="/login"
+            exact
+            component={() => <Login auth={auth} setAuth={setAuth} />}
+          />
+          <Route component={AuthContainer} />
         </Switch>
       </Router>
     </div>
